874ae6d52fba2c960049379df8b76fba8b54400c
Margot Konig <margot.konig@blackrock.com>
2019-05-15 17:58:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,8 +18,14 @@ the_manifest = {
 end
 
 def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
 
-shipping_manifest["oil painting"]
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+shipping_manifest["oil paintings"]
 end
 
 def adding
